c36b9d8cc8dc8785aee2b8ad9b6176a6e3b775d4
Najaf Ali <ali.najaf@gmail.com>
2011-05-09 19:41:58 +0100

    got rid of all the selfs

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,76 +1,77 @@
 class Golf
-  def self.hole1 a
-    a.reduce 1,:*
-  end
-  def self.hole2 s
-    s.split(' ').sort_by { |a| a[1] }.join ' '
-  end
-
-  def self.hole3 n
-    (1..n).inject :*
-  end
-  
-  def self.hole4 a
-    a.map do |e|
-      case e
-        when /man\((\w+)\)/   : "hat(man(#{$1}))"
-        when /dog\((\w+)\)/   : "dog(#{$1}(bone))"
-        when /cat\((\w+)\)/   : "dead(#{$1})"
-      end
+  class << self
+    def hole1 a
+      a.reduce 1,:*
     end
-  end
-  
-  def self.hole7(ints)
-    ranges = []
-    ints.each do |int|
-      found = false
-      ranges = ranges.map do |range|
-        if int == range.begin - 1
-          found = true
-          (range.begin - 1)..(range.end)
-        elsif int == range.end + 1
-          found = true
-          (range.begin)..(range.end + 1)
-        else
-          range
+    def hole2 s
+      s.split(' ').sort_by { |a| a[1] }.join ' '
+    end
+
+    def hole3 n
+      (1..n).inject :*
+    end
+    
+    def hole4 a
+      a.map do |e|
+        case e
+          when /man\((\w+)\)/   : "hat(man(#{$1}))"
+          when /dog\((\w+)\)/   : "dog(#{$1}(bone))"
+          when /cat\((\w+)\)/   : "dead(#{$1})"
         end
       end
-      ranges << (int..int) unless found
     end
-    ranges.map do |range|
-      range.begin == range.end ? range.begin.to_s : "#{range.begin}-#{range.end}"
+    
+    def hole7(ints)
+      ranges = []
+      ints.each do |int|
+        found = false
+        ranges = ranges.map do |range|
+          if int == range.begin - 1
+            found = true
+            (range.begin - 1)..(range.end)
+          elsif int == range.end + 1
+            found = true
+            (range.begin)..(range.end + 1)
+          else
+            range
+          end
+        end
+        ranges << (int..int) unless found
+      end
+      ranges.map do |range|
+        range.begin == range.end ? range.begin.to_s : "#{range.begin}-#{range.end}"
+      end
+    end
+    
+    def hole6(n)
+      (1..n).map{|i|
+        s = "#{'fizz' if i % 3 == 0}#{'buzz' if i % 5 == 0}"
+        s == '' ? i : s
+      }
+    end
+    
+    def hole8 n
+      l = [1, 1]
+      3.upto(n) { |i|
+        l << l[i - 2] + l[i - 3]
+      }
+      l
+    end
+    
+    def hole9(f)
+      r = File.readlines(f)
+      e = []
+      loop {
+        v = Hash.new(0)
+        r.each {|s| v[(s.gsub(/ |\n/, "").split(",") - e)[0]] += 1}
+        x = v.sort_by(&:last)
+        tc       = x[-1][0]
+        tcv = x[-1][-1]
+        bc    = x[0][0]
+        return tc if tcv > r.length / 2
+        bc ? e << bc :(return tc)
+      }
     end
   end
-  
-  def self.hole6(n)
-    (1..n).map{|i|
-      s = "#{'fizz' if i % 3 == 0}#{'buzz' if i % 5 == 0}"
-      s == '' ? i : s
-    }
-  end
-  
-  def self.hole8 n
-    l = [1, 1]
-    3.upto(n) { |i|
-      l << l[i - 2] + l[i - 3]
-    }
-    l
-  end
-  
-  def self.hole9(f)
-    r = File.readlines(f)
-    e = []
-    loop {
-      v = Hash.new(0)
-      r.each {|s| v[(s.gsub(/ |\n/, "").split(",") - e)[0]] += 1}
-      x = v.sort_by(&:last)
-      tc       = x[-1][0]
-      tcv = x[-1][-1]
-      bc    = x[0][0]
-      return tc if tcv > r.length / 2
-      bc ? e << bc :(return tc)
-    }
-  end
-
 end
 
