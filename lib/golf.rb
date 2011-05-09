c26cb573083f421e2e7d2e3336b9cd2e94c0a22a
Jordi Noguera <jordi@Jordi-Nogueras-MacBook-Pro.local>
2011-05-09 19:13:19 +0100

    hole8

--- lib/golf.rb
+++ lib/golf.rb
@@ -3,4 +3,12 @@ class Golf
   def self.hole2 s
     s.split(' ').sort { |a,b| a[1] <=> b[1] }.join ' '
   end
+
+  def self.hole8 n
+    l = [1, 1]
+    3.upto(n) { |i|
+      l << l[i - 2] + l[i - 3]
+    }
+    l
+  end
 end
