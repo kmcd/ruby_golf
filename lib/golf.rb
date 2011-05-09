784fa3d7fc9f0d46e6fc9f64eed652fb21de586e
Daniel Lucraft <dan@fluentradical.com>
2011-05-09 19:00:34 +0100

    Implement Hole 7

--- lib/golf.rb
+++ lib/golf.rb
@@ -3,4 +3,30 @@ class Golf
   def self.hole2 s
     s.split(' ').sort { |a,b| a[1] <=> b[1] }.join ' '
   end
+  
+  def self.hole7(ints)
+    ranges = []
+    ints.each do |int|
+      found = false
+      ranges = ranges.map do |range|
+        if int == range.begin - 1
+          found = true
+          (range.begin - 1)..(range.end)
+        elsif int == range.end + 1
+          found = true
+          (range.begin)..(range.end + 1)
+        else
+          range
+        end
+      end
+      ranges << (int..int) unless found
+    end
+    ranges.map do |range|
+      if range.begin == range.end
+        range.begin.to_s
+      else
+        "#{range.begin}-#{range.end}"
+      end
+    end
+  end
 end
